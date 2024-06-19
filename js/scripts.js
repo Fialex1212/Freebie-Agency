ed119843ddda66c114ca3b43e4169051949c7734
Alex <110390382+Fialex1212@users.noreply.github.com>
2024-06-19 04:24:16 +0300

    5th commit

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,26 @@ const openMenu = () =>{
     ul = document.querySelector(".header__list")
     ul.classList.toggle('active')
 };
+
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function plusSlides(n){
+    showSlide(slideIndex += n)
+}
+
+function currentSlide(n){
+    showSlide(slideIndex = n)
+}
+
+function showSlide(){
+    let i;
+    let slides = document.getElementsByClassName("mySlide");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";  
+}
+
